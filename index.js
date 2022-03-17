5d75bca3e89e7d83313d501de3867a12c6d9e43b
shota <tontonka2@gmail.com>
2022-03-17 12:45:06 +0900

    add delete method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require("express");
 const app = express();
+const activities = require("./activity.json");
 
 app.use(express.urlencoded({ extended: true}));
 
@@ -13,6 +14,16 @@ app.post("/autumn", (req, res) => {
   req.body.activity, () => {
     res.send("投稿完了");
   });
+});
+
+app.post("/update", (req, res) => {
+  activities[0].activity = req.body.updatedActivity;
+  res.send(activities);
+});
+
+app.post("/delete", (req, res) => {
+  activities.splice(req.body.number, 1);
+  res.send(activities);
 });
 
 app.listen(5000, function() {
